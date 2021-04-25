e94b1936d8e175254079084bed50e9b2f00a5ccc
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-04-25 13:41:49 +0530

    simplify using lambda

--- java8lambdabasiscs/src/io/java8/unit2/ClosureExample.java
+++ java8lambdabasiscs/src/io/java8/unit2/ClosureExample.java
@@ -6,19 +6,7 @@ public class ClosureExample {
     public static void main(String[] args) {
         int a=10;
         int b=40;//PreJava8  this should be final
-        doProcess(a, new Process(){
-
-            @Override
-            public void process(int i) {
-                //Closure example 
-                //b=60;
-                //would result in an error 
-                    //Local variable b defined in an enclosing scope must be final or effectively finalJava(536871575)
-                System.out.println(i+b);
-                
-            }
-            
-        });
+        doProcess(a, (i) -> System.out.println(i+b));
     }
     public static void doProcess(int i,Process p){
         p.process(i);
